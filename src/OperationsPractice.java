66ca17bc5c4637c00cd0a3d65c21a39495ad0c53
amruthagandham <43002735+amruthagandham@users.noreply.github.com>
2018-10-01 08:32:59 -0400

    Add files via upload

---
+++ src/OperationsPractice.java
@@ -0,0 +1,23 @@
+
+public class OperationsPractice {
+
+	public static void main(String[] args) 
+	{int number; //Data type then variable name
+	number = 7;
+	System.out.println(number);
+	number = number + 5;
+	System.out.println("added 5:" +  number);
+	number = number /2;
+	System.out.println("divided by 2:" + number);
+	number = number *4;
+	System.out.println("multiplied by 4:" + number);
+	number = number - 6;
+	System.out.println("subtracted 6:" + number);
+		number = number % 2;
+	System.out.println("modularly divide it by 2:" + number);
+	
+	
+
+	}
+
+}
